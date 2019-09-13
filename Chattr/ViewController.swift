4ac3d8178db262011d2eb374aab02c50292a3dad
Jungne <jungne7@gmail.com>
2019-09-13 15:02:46 +0200

    Initial Commit

---
+++ Chattr/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chattr
+//
+//  Created by Jungne Losang on 13/09/2019.
+//  Copyright © 2019 Jungne Losang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
